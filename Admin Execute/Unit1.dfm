fbdfbdb097f0369d197c543110adc2e9fa413259
hackbard <96856062+Real-hackbard@users.noreply.github.com>
2025-12-05 06:34:13 +0100

    Add files via upload

---
+++ Admin Execute/Unit1.dfm
@@ -0,0 +1,92 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  BorderIcons = [biSystemMenu]
+  BorderStyle = bsSingle
+  Caption = 'Admin Execute'
+  ClientHeight = 157
+  ClientWidth = 497
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  Position = poScreenCenter
+  OnCreate = FormCreate
+  TextHeight = 15
+  object Label1: TLabel
+    Left = 17
+    Top = 8
+    Width = 453
+    Height = 45
+    Caption = 'This Program is Administrator'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clGray
+    Font.Height = -32
+    Font.Name = 'Segoe UI'
+    Font.Style = [fsBold]
+    ParentFont = False
+  end
+  object Label2: TLabel
+    Left = 19
+    Top = 51
+    Width = 401
+    Height = 45
+    Caption = 'on any Windows Account !'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clGray
+    Font.Height = -32
+    Font.Name = 'Segoe UI'
+    Font.Style = [fsBold]
+    ParentFont = False
+  end
+  object Label3: TLabel
+    Left = 19
+    Top = 117
+    Width = 76
+    Height = 15
+    Caption = 'User Privilegs :'
+  end
+  object Label4: TLabel
+    Left = 101
+    Top = 117
+    Width = 51
+    Height = 15
+    Caption = 'Unknown'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clMaroon
+    Font.Height = -12
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ParentFont = False
+  end
+  object Label5: TLabel
+    Left = 15
+    Top = 6
+    Width = 453
+    Height = 45
+    Caption = 'This Program is Administrator'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clBlack
+    Font.Height = -32
+    Font.Name = 'Segoe UI'
+    Font.Style = [fsBold]
+    ParentFont = False
+    Transparent = True
+  end
+  object Label6: TLabel
+    Left = 17
+    Top = 49
+    Width = 401
+    Height = 45
+    Caption = 'on any Windows Account !'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clBlack
+    Font.Height = -32
+    Font.Name = 'Segoe UI'
+    Font.Style = [fsBold]
+    ParentFont = False
+    Transparent = True
+  end
+end
---
+++ Admin Execute/Unit1.pas
@@ -0,0 +1,108 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+{$R 'admin.res'}
+{$WARNINGS OFF}
+{$WARN SYMBOL_PLATFORM OFF}
+
+{$IFDEF CONDITIONALEXPRESSIONS}
+  {$IF CompilerVersion >= 22}
+    {$DEFINE STRING_IS_UNICODESTRING}
+  {$IFEND}
+{$ENDIF}
+
+{$R-}            // Disable range checking
+{$F-}            // Force Far Calls
+{$I-}            // I/O Error off
+{$B-}            // Complete Boolean Evaluation Off
+{$Q-}            // Overflow Checking Off
+
+function GetAdminSid: PSID;
+const
+  // known SIDs ... (WinNT.h)
+  SECURITY_NT_AUTHORITY: TSIDIdentifierAuthority = (Value: (0, 0, 0, 0, 0, 5));
+  // known RIDs ... (WinNT.h)
+  SECURITY_BUILTIN_DOMAIN_RID: DWORD = $00000020;
+  DOMAIN_ALIAS_RID_ADMINS: DWORD = $00000220;
+begin
+  Result := nil;
+  AllocateAndInitializeSid(SECURITY_NT_AUTHORITY, 2,
+    SECURITY_BUILTIN_DOMAIN_RID, DOMAIN_ALIAS_RID_ADMINS,
+    0, 0, 0, 0, 0, 0, Result);
+end;
+
+function IsAdmin: LongBool;
+var
+  TokenHandle: THandle;
+  ReturnLength: DWORD;
+  TokenInformation: PTokenGroups;
+  AdminSid: PSID;
+  Loop: Integer;
+begin
+  Result := False;
+  TokenHandle := 0;
+  if OpenProcessToken(GetCurrentProcess, TOKEN_QUERY, TokenHandle) then
+  try
+    ReturnLength := 0;
+    GetTokenInformation(TokenHandle, TokenGroups, nil, 0, ReturnLength);
+    TokenInformation := GetMemory(ReturnLength);
+    if Assigned(TokenInformation) then
+    try
+      if GetTokenInformation(TokenHandle, TokenGroups, TokenInformation,
+        ReturnLength, ReturnLength) then
+      begin
+        AdminSid := GetAdminSid;
+        for Loop := 0 to TokenInformation^.GroupCount - 1 do
+        begin
+          if EqualSid(TokenInformation^.Groups[Loop].Sid, AdminSid) then
+          begin
+            Result := True;
+            Break;
+          end;
+        end;
+        FreeSid(AdminSid);
+      end;
+    finally
+      FreeMemory(TokenInformation);
+    end;
+  finally
+    CloseHandle(TokenHandle);
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  if IsAdmin then
+    Label4.Caption := 'administrator'
+  else
+    Label4.Caption := 'no administrator';
+end;
+
+end.
